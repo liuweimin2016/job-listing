e1b1e998b36ef688778d0933822b0da954b7f122
liuweimin <liuweimin@vip.126.com>
2017-01-21 11:43:08 +0800

    implement part 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,4 @@ class ApplicationController < ActionController::Base
         redirect_to root_path
       end
   end
-
 end
--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -23,23 +23,27 @@ before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destro
       end
     end
 
-    private
-
-    def job_params
-      params.require(:job).permit(:title, :description)
-    end
-
     def edit
         @job = Job.find(params[:id])
       end
 
-      def update
+    def update
         @job = Job.find(params[:id])
         if @job.update(job_params)
           redirect_to jobs_path
         else
           render :edit
         end
-      end
+    end
 
+    def destroy
+          @job = Job.find(params[:id])
+          @job.destroy
+          redirect_to jobs_path
+    end
+
+    private
+    def job_params
+      params.require(:job).permit(:title, :description)
+    end
 end
