3d96c72537134d3f063805831ec2c4fb9b610be2
liuweimin <liuweimin@vip.126.com>
2017-01-22 15:55:02 +0800

    implement wage part

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
      def admin?
+       email == 'liuweimin@vip.126.com'
        is_admin
      end
 end
